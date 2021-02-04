4b90569df4f6394b2a01826e1f2fb21ec656418a
Slava <vyacheslav.pivovarov@4eck-media.de>
2021-02-04 14:49:53 +0200

    lesson_3, homework

--- lesson_3/homework/1.js
+++ lesson_3/homework/1.js
@@ -18,5 +18,109 @@
 
 */
 
-  var OurSliderImages = ['images/cat1.jpg', 'images/cat2.jpg', 'images/cat3.jpg', 'images/cat4.jpg', 'images/cat5.jpg', 'images/cat6.jpg', 'images/cat7.jpg', 'images/cat8.jpg'];
-  var currentPosition = 0;
+let OurSliderImages = ['images/cat1.jpg', 'images/cat2.jpg', 'images/cat3.jpg', 'images/cat4.jpg', 'images/cat5.jpg', 'images/cat6.jpg', 'images/cat7.jpg', 'images/cat8.jpg'];
+window.addEventListener('load', () => {
+    const Slider = {
+        // Slider data
+        sliderContainer: null,
+
+        // Slider Navigation
+        navigation: {
+            prevSlide: {
+                element: null,
+                selector: '.prev-slide',
+            },
+            nextSlide: {
+                element: null,
+                selector: '.next-slide',
+            },
+        },
+
+        // Slider Carousel
+        carousel: {
+            element: null,
+            selector: '.slider',
+            position: 0,
+            currentImage: null,
+            images: {
+                elements: [],
+                urls: [],
+                count: 0,
+            },
+        },
+
+        // General slider methods
+        init(selector) {
+            this.sliderContainer = document.querySelector(selector);
+            this.initNavigation();
+            this.initCarousel();
+        },
+
+        initNavigation() {
+            this.navigation.prevSlide.element = this.sliderContainer.querySelector(this.navigation.prevSlide.selector);
+            this.navigation.nextSlide.element = this.sliderContainer.querySelector(this.navigation.nextSlide.selector);
+            this.navigation.prevSlide.element.addEventListener('click', () => {
+                this.prevSlide()
+            });
+            this.navigation.nextSlide.element.addEventListener('click', () => {
+                this.nextSlide()
+            });
+        },
+
+        initCarousel() {
+            this.carousel.element = this.sliderContainer.querySelector(this.carousel.selector);
+            this.setImages();
+        },
+
+        setImages(images = []) {
+            this.carousel.element.innerHTML = '';
+            if (!images.length) {
+                this.carousel.element.innerHTML = "<code>No slides. Please use setImages() method</code>"
+            } else {
+                this.carousel.images.urls = images;
+                this.carousel.images.count = images.length - 1;
+                images.forEach((imageUrl, index) => {
+                    let imageElement = document.createElement('img');
+                    imageElement.src = imageUrl;
+                    this.carousel.images.elements[index] = imageElement;
+                });
+                this.carousel.currentImage = this.carousel.images.elements[this.carousel.position];
+                this.carousel.element.append(this.carousel.images.elements[this.carousel.position]);
+            }
+        },
+        prevSlide() {
+            if (this.carousel.position > 0) {
+                this.carousel.position--;
+            } else {
+                this.carousel.position = this.carousel.images.count;
+            }
+            this.changeSlide(this.carousel.position);
+        },
+
+        nextSlide() {
+            if (this.carousel.position < this.carousel.images.count) {
+                this.carousel.position++;
+            } else {
+                this.carousel.position = 0;
+            }
+            this.changeSlide(this.carousel.position);
+        },
+
+        changeSlide(position) {
+            this.carousel.currentImage.classList.add('fade-out');
+            this.carousel.currentImage.addEventListener('animationend', () => {
+                this.carousel.element.innerHTML = '';
+                this.carousel.currentImage = this.carousel.images.elements[position];
+                this.carousel.currentImage.classList.add('fade-in');
+                this.carousel.element.append(this.carousel.currentImage);
+            });
+            this.carousel.currentImage.addEventListener('animationstart', () => {
+                console.log('true')
+            });
+        },
+    };
+
+    Slider.init('.slider-wrapper');
+    Slider.setImages(OurSliderImages);
+// Slider.setImages(); // If no images comes
+});
