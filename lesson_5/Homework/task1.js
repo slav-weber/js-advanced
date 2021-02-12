338f93832285dfc3f1b0edfc7cacc496e0e9c073
Slava <vyacheslav.pivovarov@4eck-media.de>
2021-02-12 14:10:36 +0200

    Lesson 5, homework

--- lesson_5/Homework/task1.js
+++ lesson_5/Homework/task1.js
@@ -1,3 +1,5 @@
+"use strict"
+
 /*
 
   Задание:
@@ -26,3 +28,49 @@
 
 
 */
+
+let comment1 = new Comment('John', 'hey hey hey');
+let comment2 = new Comment('Nohn', 'Hi hi hi');
+let comment3 = new Comment('Pohn', 'zey hzey hzy');
+let comment4 = new Comment('Vohn', 'piy piy piy');
+
+let commentsArray = [comment1, comment2, comment3, comment4];
+
+createCommentsFeed(commentsArray);
+
+function Comment(name, text, avatarUrl) {
+    this.name = name;
+    this.text = text;
+    this.avatarUrl = avatarUrl || 'default.jpg';
+    this.likes = 0;
+
+    this.addLike = () => {
+        this.likes++;
+    }
+}
+
+function createCommentsFeed(comments) {
+    comments.forEach((comment) => {
+        let elements = {
+            container: document.createElement('div'),
+            name: document.createElement('div'),
+            text: document.createElement('div'),
+            avatar: document.createElement('img'),
+            likes: document.createElement('button'),
+        }
+
+        elements.name.innerText = `${comment.name} `;
+        elements.text.innerText = `said: ${comment.text}`;
+        elements.avatar.src = comment.avatarUrl;
+        elements.likes.innerText = comment.likes;
+        elements.likes.addEventListener('click', () => {
+            comment.addLike();
+            elements.likes.innerText = comment.likes;
+        });
+        elements.container.append(elements.avatar);
+        elements.container.append(elements.name);
+        elements.container.append(elements.text);
+        elements.container.append(elements.likes);
+        document.body.append(elements.container);
+    })
+}
